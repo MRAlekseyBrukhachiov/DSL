a36dd129628dc2bc4e3f71ec2d0d8ff2ebf3daba
MRAlekseyBrukhachiov <aleksey.bryukhachiov@yandex.ru>
2022-03-16 22:09:05 +0300

    add lexer

--- src/Lexems.java
+++ src/Lexems.java
@@ -9,7 +9,7 @@ public class Lexems {
     public Lexems() {
         lexems.put("VAR", Pattern.compile("[a-z]\\w*"));
         lexems.put("DIGIT", Pattern.compile("^0|[1-9][0-9]*"));
-        lexems.put("ASSIGNMENT OPERATOR", Pattern.compile("="));
+        lexems.put("ASSIGN_OP", Pattern.compile("="));
         lexems.put("OP", Pattern.compile("\\-|\\+|\\*|\\/"));
         //lexems.put("DO_WHILE", Pattern.compile("do.*while"));
     }
