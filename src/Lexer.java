7677567916c4eb59945d474cdf0cd4052a7d64b4
MRAlekseyBrukhachiov <aleksey.bryukhachiov@yandex.ru>
2022-05-13 01:33:40 +0300

    add print

--- src/Lexer.java
+++ src/Lexer.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 
 public class Lexer {
 
-    private String testCode;
-    private ArrayList<Token> tokens = new ArrayList<>();
-    private static Map<String, Pattern> lexems = new HashMap<>();
+    private final String testCode;
+    private final ArrayList<Token> tokens = new ArrayList<>();
+    private static final Map<String, Pattern> lexemes = new HashMap<>();
 
     public Lexer(String testCode) {
         this.testCode = testCode;
@@ -16,20 +16,21 @@ public class Lexer {
     }
 
     static {
-        lexems.put("VAR", Pattern.compile("^[a-z_]\\w*$"));
-        lexems.put("DIGIT", Pattern.compile("^\\d*$"));
-        lexems.put("ASSIGN_OP", Pattern.compile("^=$"));
-        lexems.put("OP", Pattern.compile("^(-|\\+|\\*|/)$"));
-        lexems.put("L_BC", Pattern.compile("^\\($"));
-        lexems.put("R_BC", Pattern.compile("^\\)$"));
-        lexems.put("ENDL", Pattern.compile("^;$"));
-        lexems.put("COMPARE_OP", Pattern.compile("^(~|<|>|!=)$"));
-        lexems.put("IF", Pattern.compile("^IF$"));
-        lexems.put("ELSE", Pattern.compile("^ELSE$"));
-        lexems.put("WHILE", Pattern.compile("^WHILE$"));
-        lexems.put("DO", Pattern.compile("^DO$"));
-        lexems.put("FOR", Pattern.compile("^FOR$"));
-        lexems.put("DIV", Pattern.compile("^,$"));
+        lexemes.put("VAR", Pattern.compile("^[a-z_]\\w*$"));
+        lexemes.put("DIGIT", Pattern.compile("^\\d*$"));
+        lexemes.put("ASSIGN_OP", Pattern.compile("^=$"));
+        lexemes.put("OP", Pattern.compile("^(-|\\+|\\*|/)$"));
+        lexemes.put("L_BC", Pattern.compile("^\\($"));
+        lexemes.put("R_BC", Pattern.compile("^\\)$"));
+        lexemes.put("ENDL", Pattern.compile("^;$"));
+        lexemes.put("COMPARE_OP", Pattern.compile("^(~|<|>|!=)$"));
+        lexemes.put("IF", Pattern.compile("^IF$"));
+        lexemes.put("ELSE", Pattern.compile("^ELSE$"));
+        lexemes.put("WHILE", Pattern.compile("^WHILE$"));
+        lexemes.put("DO", Pattern.compile("^DO$"));
+        lexemes.put("FOR", Pattern.compile("^FOR$"));
+        lexemes.put("DIV", Pattern.compile("^,$"));
+        lexemes.put("PRINT", Pattern.compile("^PRINT$"));
     }
 
     private void run() {
@@ -47,8 +48,8 @@ public class Lexer {
                 tokenEnd = tokenStart + testCode.toCharArray()[i + 1];
             }
 
-            for (String key: lexems.keySet()) {
-                Pattern p = lexems.get(key);
+            for (String key: lexemes.keySet()) {
+                Pattern p = lexemes.get(key);
                 Matcher m_1 = p.matcher(tokenStart);
                 Matcher m_2 = p.matcher(tokenEnd);
 
