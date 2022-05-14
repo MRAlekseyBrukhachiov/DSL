4612daa88dec45187e6876e1bee4a61ca61634d8
MRAlekseyBrukhachiov <aleksey.bryukhachiov@yandex.ru>
2022-05-14 23:59:31 +0300

    update Interpreter

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class Interpreter {
 
     private final ArrayList<Token> infixExpr;
     private final Map<String, Double> variables = new HashMap<>();
+
+    private int iterator;
+    private Token cur;
+    private boolean transCondition;
 
     private int operationPriority(Token op) {
         return switch (op.getToken()) {
@@ -27,7 +28,7 @@ public class Interpreter {
         };
     }
 
-    public boolean compare(Token comp, double first, double second) {
+    private boolean compare(Token comp, double first, double second) {
         return switch (comp.getToken()) {
             case ">" -> Double.compare(first, second) == 1;
             case "~" -> Double.compare(first, second) == 0;
@@ -39,208 +40,208 @@ public class Interpreter {
 
     public Interpreter(ArrayList<Token> infixExpr) {
         this.infixExpr = infixExpr;
+        cur = infixExpr.get(0);
+        iterator = 0;
+        transCondition = false;
         run();
     }
 
-    private void run() {
-        int temp;
-        int indexVar;
-        int comparison_op_index;
-        double first;
-        double second;
-        for (int i = 0; i < infixExpr.size(); i++) {
-            Token cur = infixExpr.get(i);
-            if (cur.getType() == "ASSIGN_OP") {
-                indexVar = i - 1;
-                temp = i + 1;
-                while (cur.getType() != "ENDL") {
-                    i++;
-                    cur = infixExpr.get(i);
-                }
-                double rez = calc(toPostfix(infixExpr, temp, i));
+    private void interpret_value(String trans) {
+        int indexVar = iterator - 1;
+        int startExpr = iterator + 1;
+        while (!trans.equals(cur.getType())) {
+            if ("DIV".equals(cur.getType())) {
+                double rez = calc(toPostfix(infixExpr, startExpr, iterator));
                 variables.put(infixExpr.get(indexVar).getToken(), rez);
+                indexVar = iterator + 1;
+                startExpr = iterator + 3;
             }
-            if (cur.getType() == "IF") {
-                int first_argument_index = i + 2;
-                int second_argument_index = i + 4;
-                comparison_op_index = i + 3;
-                second = 0;
-                first = variables.get(infixExpr.get(first_argument_index).getToken());
-                if (infixExpr.get(second_argument_index).getType() == "DIGIT") {
-                    second = Double.parseDouble(infixExpr.get(second_argument_index).getToken());
-                }
-                if (infixExpr.get(second_argument_index).getType() == "VAR") {
-                    second = variables.get(infixExpr.get(second_argument_index).getToken());
-                }
-                i += 6;
-                cur = infixExpr.get(i);
-                if (!compare(infixExpr.get(comparison_op_index), first, second)) {
-                    while (cur.getType() != "ENDL") {
-                        if (cur.getType() == "ELSE") {
-                            break;
-                        }
-                        i++;
-                        cur = infixExpr.get(i);
-                    }
-                } else {
-                    indexVar = i;
-                    temp = i + 2;
-                    while (cur.getType() != "ELSE") {
-                        i++;
-                        cur = infixExpr.get(i);
-                    }
-                    double rez = calc(toPostfix(infixExpr, temp, i));
-                    variables.put(infixExpr.get(indexVar).getToken(), rez);
-                    while (cur.getType() != "ENDL") {
-                        i++;
-                        cur = infixExpr.get(i);
-                    }
-                }
-            }
-            if (cur.getType() == "WHILE") {
-                int first_argument_index = i + 2;
-                int second_argument_index = i + 4;
-                comparison_op_index = i + 3;
-                second = 0;
-                first = variables.get(infixExpr.get(first_argument_index).getToken());
-                if (infixExpr.get(second_argument_index).getType() == "DIGIT") {
-                    second = Double.parseDouble(infixExpr.get(second_argument_index).getToken());
-                }
-                if (infixExpr.get(second_argument_index).getType() == "VAR") {
-                    second = variables.get(infixExpr.get(second_argument_index).getToken());
-                }
-                i += 6;
-                cur = infixExpr.get(i);
-                int start_iteration = i;
-                while (compare(infixExpr.get(comparison_op_index), first, second)) {
-                    indexVar = i;
-                    temp = i + 2;
-                    while (cur.getType() != "ENDL") {
-                        i++;
-                        cur = infixExpr.get(i);
-                    }
-                    double rez = calc(toPostfix(infixExpr, temp, i));
-                    variables.put(infixExpr.get(indexVar).getToken(), rez);
-                    i = start_iteration;
-                    cur = infixExpr.get(i);
-                    first = variables.get(cur.getToken());
-                }
-                while (cur.getType() != "ENDL") {
-                    i++;
-                    cur = infixExpr.get(i);
-                }
-            }
-            if (cur.getType() == "DO") {
-                i++;
-                int start_iteration = i;
-                do {
-                    indexVar = i;
-                    temp = i + 2;
-                    while (cur.getType() != "WHILE") {
-                        i++;
-                        cur = infixExpr.get(i);
-                    }
-                    double rez = calc(toPostfix(infixExpr, temp, i));
-                    variables.put(infixExpr.get(indexVar).getToken(), rez);
+            iterator++;
+            cur = infixExpr.get(iterator);
+        }
+        double rez = calc(toPostfix(infixExpr, startExpr, iterator));
+        variables.put(infixExpr.get(indexVar).getToken(), rez);
+    }
 
-                    int first_argument_index = i + 2;
-                    int second_argument_index = i + 4;
-                    comparison_op_index = i + 3;
-                    second = 0;
-                    first = variables.get(infixExpr.get(first_argument_index).getToken());
-                    if (infixExpr.get(second_argument_index).getType() == "DIGIT") {
-                        second = Double.parseDouble(infixExpr.get(second_argument_index).getToken());
-                    }
-                    if (infixExpr.get(second_argument_index).getType() == "VAR") {
-                        second = variables.get(infixExpr.get(second_argument_index).getToken());
-                    }
-                    i = start_iteration;
-                    cur = infixExpr.get(i);
-                } while (compare(infixExpr.get(comparison_op_index), first, second));
+    private void interpret_condition() {
+        int first_argument_index = iterator + 2;
+        int comparison_op_index = iterator + 3;
+        int second_argument_index = iterator + 4;
+        Token s = infixExpr.get(second_argument_index);
+        double first = variables.get(infixExpr.get(first_argument_index).getToken());
+        double second = switch (s.getType()) {
+            case "DIGIT" -> Double.parseDouble(s.getToken());
+            case "VAR" -> variables.get(s.getToken());
+            default ->  0.0;
+        };
 
-                while (cur.getType() != "ENDL") {
-                    i++;
-                    cur = infixExpr.get(i);
+        iterator += 6;
+        cur = infixExpr.get(iterator);
+
+        transCondition = compare(infixExpr.get(comparison_op_index), first, second);
+    }
+
+    private void interpret_if() {
+        interpret_condition();
+        if (!transCondition) {
+            while (!"ENDL".equals(cur.getType())) {
+                if ("ELSE".equals(cur.getType())) {
+                    break;
                 }
+                iterator++;
+                cur = infixExpr.get(iterator);
+            }
+        } else {
+            iterator++;
+            interpret_value("ELSE");
+            while (!"ENDL".equals(cur.getType())) {
+                iterator++;
+                cur = infixExpr.get(iterator);
+            }
+        }
+    }
+
+    private void interpret_while() {
+        int start_iteration = iterator;
+        interpret_condition();
+        while (transCondition) {
+            iterator++;
+            interpret_value("ENDL");
+            iterator = start_iteration;
+            cur = infixExpr.get(iterator);
+            interpret_condition();
+        }
+        while (!"ENDL".equals(cur.getType())) {
+            iterator++;
+            cur = infixExpr.get(iterator);
+        }
+    }
+
+    private void interpret_do_while() {
+        iterator += 2;
+        cur = infixExpr.get(iterator);
+        int start_iteration = iterator;
+        do {
+            interpret_value("WHILE");
+            interpret_condition();
+            iterator = start_iteration;
+            cur = infixExpr.get(iterator);
+        } while (transCondition);
+
+        while (!"ENDL".equals(cur.getType())) {
+            iterator++;
+            cur = infixExpr.get(iterator);
+        }
+    }
+
+    private void interpret_for() {
+        iterator += 3;
+        cur = infixExpr.get(iterator);
+        interpret_value("DIV");
+
+        iterator--;
+        int condition = iterator;
+        interpret_condition();
+
+        int indexAfterFor = iterator + 1;
+        while (transCondition) {
+            while (!"R_BC".equals(cur.getType())) {
+                iterator++;
+                cur = infixExpr.get(iterator);
+            }
+            iterator += 2;
+            cur = infixExpr.get(iterator);
+            interpret_value("ENDL");
+
+            iterator = indexAfterFor;
+            cur = infixExpr.get(iterator);
+            interpret_value("R_BC");
+
+            iterator = condition;
+            interpret_condition();
+        }
+
+        while (!"ENDL".equals(cur.getType())) {
+            iterator++;
+            cur = infixExpr.get(iterator);
+        }
+    }
+
+    private void interpret_print() {
+        iterator++;
+        cur = infixExpr.get(iterator);
+        if ("L_BC".equals(cur.getType())) {
+            Token c = infixExpr.get(iterator + 1);
+            switch (c.getType()) {
+                case "DIGIT" -> System.out.println(Double.parseDouble(c.getToken()));
+                case "VAR" -> System.out.println(variables.get(c.getToken()));
+            }
+            iterator += 2;
+        } else {
+            System.out.println(variables);
+        }
+    }
+
+    private void run() {
+        for (; iterator < infixExpr.size(); iterator++) {
+            cur = infixExpr.get(iterator);
+            switch (cur.getType()) {
+                case "ASSIGN_OP" -> interpret_value("ENDL");
+                case "IF" -> interpret_if();
+                case "WHILE" -> interpret_while();
+                case "DO" -> interpret_do_while();
+                case "FOR" -> interpret_for();
+                case "PRINT" -> interpret_print();
             }
         }
     }
 
     private ArrayList<Token> toPostfix(ArrayList<Token> infixExpr, int start, int end) {
-
-        //	Выходная строка, содержащая постфиксную запись
-        ArrayList<Token> postfixExpr = new ArrayList<>();
-
-        //	Инициализация стека, содержащий операторы в виде символов
-        Stack<Token> stack = new Stack<>();
-
+        ArrayList<Token> postfixExpr = new ArrayList<>(); // Выходная строка, содержащая постфиксную запись
+        Stack<Token> stack = new Stack<>(); // Инициализация стека, содержащий операторы в виде символов
         //	Перебираем строку
         for (int i = start; i < end; i++) {
-            //	Текущий символ
-            Token c = infixExpr.get(i);
-
-            //	Если симовол - цифра
-            if (c.getType() == "DIGIT" || c.getType() == "VAR") {
-                postfixExpr.add(c);
-            } else if (c.getType() == "L_BC") { //	Если открывающаяся скобка
-                //	Заносим её в стек
-                stack.push(c);
-            } else if (c.getType() == "R_BC") {//	Если закрывающая скобка
-                //	Заносим в выходную строку из стека всё вплоть до открывающей скобки
-                while (stack.size() > 0 && stack.peek().getType() != "L_BC")
-                    postfixExpr.add(stack.pop());
-                //	Удаляем открывающуюся скобку из стека
-                stack.pop();
-            } else if (c.getType() == "OP") { //	Проверяем, содержится ли символ в списке операторов
-                Token op = c;
-                //	Заносим в выходную строку все операторы из стека, имеющие более высокий приоритет
-                while (stack.size() > 0 && (operationPriority(stack.peek()) >= operationPriority(op)))
-                    postfixExpr.add(stack.pop());
-                //	Заносим в стек оператор
-                stack.push(c);
+            Token c = infixExpr.get(i); // Текущий токен
+            switch (c.getType()) {
+                case "DIGIT", "VAR" -> postfixExpr.add(c); // Если токен - цифра
+                case "L_BC" -> stack.push(c); // Если открывающаяся скобка заносим её в стек
+                case "R_BC" -> { //	Если закрывающая скобка
+                    //	Заносим в выходную строку из стека всё вплоть до открывающей скобки
+                    while (stack.size() > 0 && !"L_BC".equals(stack.peek().getType()))
+                        postfixExpr.add(stack.pop());
+                    stack.pop(); //	Удаляем открывающуюся скобку из стека
+                }
+                case "OP" -> {  //	Проверяем, содержится ли символ в списке операторов
+                    Token op = c;
+                    while (stack.size() > 0 && (operationPriority(stack.peek()) >= operationPriority(op))) {
+                        postfixExpr.add(stack.pop());
+                    } // Заносим в выходную строку все операторы из стека, имеющие более высокий приоритет
+                    stack.push(c); // Заносим в стек оператор
+                }
             }
         }
 
-        //	Заносим все оставшиеся операторы из стека в выходную строку
         while (!stack.isEmpty()) {
             postfixExpr.add(stack.pop());
-        }
+        } // Заносим все оставшиеся операторы из стека в выходную строку
 
-        //	Возвращаем выражение в постфиксной записи
         return postfixExpr;
     }
 
     private double calc(ArrayList<Token> postfixExpr) {
-        //	Стек для хранения чисел
-        Stack<Double> locals = new Stack<>();
-        //	Счётчик действий
-        int counter = 0;
-
-        //	Проходим по строке
+        Stack<Double> locals = new Stack<>(); // Стек для хранения чисел
         for (int i = 0; i < postfixExpr.size(); i++) {
-            //	Текущий символ
             Token c = postfixExpr.get(i);
-
-            //	Если символ число
-            if (c.getType() == "DIGIT") {
-                String number = c.getToken();
-                locals.push(Double.parseDouble(number));
-            } else if (c.getType() == "VAR") {
-                locals.push(variables.get(c.getToken()));
-            } else if (c.getType() == "OP") { //	Если символ есть в списке операторов
-                //	Прибавляем значение счётчику
-                counter += 1;
-
-                //	Получаем значения из стека в обратном порядке
-                double second = locals.size() > 0 ? locals.pop() : 0,
-                        first = locals.size() > 0 ? locals.pop() : 0;
-
-                //	Получаем результат операции и заносим в стек
-                locals.push(execute(c, first, second));
+            switch (c.getType()) { // Если символ число
+                case "DIGIT" -> locals.push(Double.parseDouble(c.getToken()));
+                case "VAR" -> locals.push(variables.get(c.getToken()));
+                case "OP" -> { // Получаем значения из стека в обратном порядке
+                    double second = locals.size() > 0 ? locals.pop() : 0,
+                            first = locals.size() > 0 ? locals.pop() : 0;
+                    locals.push(execute(c, first, second)); // Получаем результат операции и заносим в стек
+                }
             }
         }
-
-        //	По завершению цикла возвращаем результат из стека
         return locals.pop();
     }
 
